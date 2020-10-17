b9e1b2bbeddac742127ee715ff10242706d90528
Willian Ramos de Melo - WME <willian.melo@iteris.com.br>
2020-10-17 19:18:01 -0300

    Iniciando Projeto

---
+++ TCC/Bela-City/src/app/menu-layout/menu-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-layout',
+  templateUrl: './menu-layout.component.html',
+  styleUrls: ['./menu-layout.component.css']
+})
+export class MenuLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
